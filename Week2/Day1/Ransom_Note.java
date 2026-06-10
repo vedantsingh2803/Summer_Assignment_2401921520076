5b42a52673e11318ffcf0c702b59038fb2b4bfe9
Bedant Singh <bedantsingh100@gmail.com>
2026-06-10 17:30:11 +0530

    Added Week 2 Day 1

---
+++ Week2/Day1/Ransom_Note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        for (char ch : magazine.toCharArray()) {
+            count[ch - 'a']++;
+        }
+
+        for (char ch : ransomNote.toCharArray()) {
+            if (count[ch - 'a'] == 0) {
+                return false;
+            }
+            count[ch - 'a']--;
+        }
+
+        return true;
+    }
+}
