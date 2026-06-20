833003ff50b477658b242da397a43bbe071f944d
Bedant Singh <bedantsingh100@gmail.com>
2026-06-20 23:21:27 +0530

    Added oops week 3

---
+++ Week3/OOPS/TestCompartment.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+
+abstract class Compartment {
+    public abstract String notice();
+}
+
+class FirstClass extends Compartment {
+    public String notice() {
+        return "First Class Compartment";
+    }
+}
+
+class Ladies extends Compartment {
+    public String notice() {
+        return "Ladies Compartment";
+    }
+}
+
+class General extends Compartment {
+    public String notice() {
+        return "General Compartment";
+    }
+}
+
+class Luggage extends Compartment {
+    public String notice() {
+        return "Luggage Compartment";
+    }
+}
+
+public class TestCompartment {
+    public static void main(String[] args) {
+        Compartment[] compartments = new Compartment[10];
+        Random random = new Random();
+
+        for (int i = 0; i < compartments.length; i++) {
+            int choice = random.nextInt(4) + 1;
+
+            switch (choice) {
+                case 1:
+                    compartments[i] = new FirstClass();
+                    break;
+                case 2:
+                    compartments[i] = new Ladies();
+                    break;
+                case 3:
+                    compartments[i] = new General();
+                    break;
+                case 4:
+                    compartments[i] = new Luggage();
+                    break;
+            }
+        }
+
+        for (Compartment c : compartments) {
+            System.out.println(c.notice());
+        }
+    }
+}
