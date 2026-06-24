2de92fae12a1a2d114495706c71bfd51184fba1c
Bedant Singh <bedantsingh100@gmail.com>
2026-06-24 10:01:49 +0530

    Week 4 Day 3

---
+++ Week4/Day3/ValidateBinarySearchTree.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return check(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean check(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+
+        return check(node.left, min, node.val) &&
+                check(node.right, node.val, max);
+    }
+}
